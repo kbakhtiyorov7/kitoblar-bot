5ca6b126d1f96c46d38cc4a34d3292c616580725
kbakhtiyorov7 <kbaxtiyorov10@gmail.com>
2026-02-07 12:22:41 +0500

    free

--- middlewares/__init__.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Dispatcher
-
-from .throttling import ThrottlingMiddleware
-
-
-def setup_middlewares(dp: Dispatcher) -> None:
-    """Barcha middlewarelarni sozlash"""
-    # Throttling middleware
-    dp.message.middleware(ThrottlingMiddleware(limit=0.5))
--- utils/db_api/__init__.py
+++
@@ -1,3 +0,0 @@
-# Database API modullari shu yerga import qilinadi
-# Masalan:
-# from .postgresql import Database
