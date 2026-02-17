0a013b79d4ffbb8c03587f91f233023cb8fa6bc5
kbakhtiyorov7 <kbaxtiyorov520@gmail.com>
2026-02-17 09:48:29 +0500

    upgrate

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/bakhtiyorov/Telegram-Bot/Projects/Kitoblar-bot/kitoblar-bot/venv
+set -gx VIRTUAL_ENV /home/bakhtiyorov/Telegram-bot/Projects/Kitoblar-Bot/kitoblar-bot/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
